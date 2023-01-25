03e4e5eb55d9168bf9d7be74efee6f053bd49724
Ivan Todorov <ivantodorov1607@gmail.com>
2023-01-25 17:58:18 +0200

    almost finished section 14, need to watch wrap up vid 183

--- src/App.js
+++ src/App.js
@@ -13,22 +13,25 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://console.firebase.google.com/u/0/project/react-htto/database/react-htto-default-rtdb/data/~2F');
+      const response = await fetch('https://react-htto-default-rtdb.europe-west1.firebasedatabase.app/movies.json');
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
 
       const data = await response.json();
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedMovies = [];
+
+      for(const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate
+        });
+      }
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -39,8 +42,17 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
+  async function addMovieHandler(movie) {
+    const response = await fetch('https://react-htto-default-rtdb.europe-west1.firebasedatabase.app/movies.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      }
+    });
+    const data = await response.json();
+    console.log(data)
   }
 
   let content = <p>Found no movies.</p>;

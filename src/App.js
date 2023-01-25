d1071a708c41883ad279ec09401ba3d1ade3e704
Ivan Todorov <ivantodorov1607@gmail.com>
2023-01-25 14:21:30 +0200

    initial setup section 14

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React, { useState } from 'react';
+import React from 'react';
 
+import MoviesList from './components/MoviesList';
 import './App.css';
-import Button from "./components/UI/Button/Button";
 
 function App() {
-    const [showParagraph, setShowParagraph] = useState(false);
-
-    const toggleParagraphHandler = () => {
-        setShowParagraph(prevShowParagraph => !prevShowParagraph)
-    }
+  const dummyMovies = [
+    {
+      id: 1,
+      title: 'Some Dummy Movie',
+      openingText: 'This is the opening text of the movie',
+      releaseDate: '2021-05-18',
+    },
+    {
+      id: 2,
+      title: 'Some Dummy Movie 2',
+      openingText: 'This is the second opening text of the movie',
+      releaseDate: '2021-05-19',
+    },
+  ];
 
   return (
-    <div className="app">
-      <h1>Hi there!</h1>
-        {showParagraph && <p>This is new!</p>}
-        <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
-    </div>
+    <React.Fragment>
+      <section>
+        <button>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={dummyMovies} />
+      </section>
+    </React.Fragment>
   );
 }
 

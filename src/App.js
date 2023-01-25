458aa569e70e26025da287d48d6eb2fd77aead89
Ivan Todorov <ivantodorov1607@gmail.com>
2023-01-25 21:31:15 +0200

    Initial setup for section 15

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import Header from "./components/Layout/Header";
-import { useState } from "react";
-import Meals from "./components/Meals/Meals";
-import Cart from "./components/Cart/Cart";
-import CartProvider from "./store/CartProvider";
+import React from 'react';
+import BackwardCounter from './components/BackwardCounter';
+import ForwardCounter from './components/ForwardCounter';
+
 function App() {
-    const [cartIsShown, setCartIsShown] = useState(false);
-
-    const showCartHandler = () => {
-        setCartIsShown(true);
-    }
-    const hideCartHandler = () => {
-        setCartIsShown(false);
-    }
-
   return (
-      <CartProvider>
-          {cartIsShown && <Cart onClose={hideCartHandler}/>}
-          <Header onShowCart={showCartHandler}/>
-          <main>
-              <Meals/>
-          </main>
-      </CartProvider>
+    <React.Fragment>
+      <ForwardCounter />
+      <BackwardCounter />
+    </React.Fragment>
   );
 }
 

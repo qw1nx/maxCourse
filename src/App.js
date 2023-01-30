1f6756ff607fab4f25bba4586e27cff8431dc43f
Ivan Todorov <ivantodorov1607@gmail.com>
2023-01-30 16:03:53 +0200

    nitial setup for the redux section

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import Header from "./components/Layout/Header";
-import { useState } from "react";
-import Meals from "./components/Meals/Meals";
-import Cart from "./components/Cart/Cart";
-import CartProvider from "./store/CartProvider";
+import Counter from './components/Counter';
+
+
 function App() {
-    const [cartIsShown, setCartIsShown] = useState(false);
-
-    const showCartHandler = () => {
-        setCartIsShown(true);
-    }
-    const hideCartHandler = () => {
-        setCartIsShown(false);
-    }
-
   return (
-      <CartProvider>
-          {cartIsShown && <Cart onClose={hideCartHandler}/>}
-          <Header onShowCart={showCartHandler}/>
-          <main>
-              <Meals/>
-          </main>
-      </CartProvider>
+    <Counter />
   );
 }
 

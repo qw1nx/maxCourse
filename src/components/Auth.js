73352fd84bfdb0fe1ead8367c714473bf550ef94
Ivan Todorov <ivantodorov1607@gmail.com>
2023-02-01 14:00:39 +0200

    finished section 18

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,6 +1,18 @@
 import classes from './Auth.module.css';
+import {useDispatch, useSelector} from "react-redux";
+import {authActions} from "../store/auth";
 
 const Auth = () => {
+
+  const dispatch = useDispatch();
+
+  const login = (event) => {
+    event.preventDefault();
+    dispatch( authActions.login())
+  }
+
+
+
   return (
     <main className={classes.auth}>
       <section>
@@ -13,7 +25,7 @@ const Auth = () => {
             <label htmlFor='password'>Password</label>
             <input type='password' id='password' />
           </div>
-          <button>Login</button>
+          <button onClick={login}>Login</button>
         </form>
       </section>
     </main>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,29 @@
 import classes from './Header.module.css';
+import {useDispatch, useSelector} from "react-redux";
+import {authActions} from "../store/auth";
 
 const Header = () => {
+
+    const dispatch = useDispatch();
+    const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+    const logout = () => {
+      dispatch( authActions.logout())
+    }
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
       <nav>
         <ul>
           <li>
-            <a href='/'>My Products</a>
+            {isAuthenticated && <a href='/'>My Products</a>}
           </li>
           <li>
-            <a href='/'>My Sales</a>
+            {isAuthenticated && <a href='/'>My Sales</a>}
           </li>
           <li>
-            <button>Logout</button>
+            {isAuthenticated && <button onClick={logout}>Logout</button>}
           </li>
         </ul>
       </nav>

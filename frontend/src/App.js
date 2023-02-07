24bab64fddcb58003a403a74a58608928236c6e3
Ivan Todorov <ivantodorov1607@gmail.com>
2023-02-07 17:08:10 +0200

    starting out

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,16 @@
 // Challenge / Exercise
 
 // 1. Add five new (dummy) page components (content can be simple <h1> elements)
-//    - HomePage
+//    - HomePage.js
 //    - EventsPage
 //    - EventDetailPage
-//    - NewEventPage
+//    - NewEventPage.js
 //    - EditEventPage
 // 2. Add routing & route definitions for these five pages
-//    - / => HomePage
+//    - / => HomePage.js
 //    - /events => EventsPage
 //    - /events/<some-id> => EventDetailPage
-//    - /events/new => NewEventPage
+//    - /events/new => NewEventPage.js
 //    - /events/<some-id>/edit => EditEventPage
 // 3. Add a root layout that adds the <MainNavigation> component above all page components
 // 4. Add properly working links to the MainNavigation
@@ -20,8 +20,26 @@
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
+import { RouterProvider, createBrowserRouter} from 'react-router-dom'
+import HomePage from "./pages/HomePage";
+import EventsPage from "./pages/EventsPage";
+import EventDetailPage from "./pages/EventDetailPage";
+import NewEventPage from "./pages/NewEventPage";
+import EditEventPage from "./pages/EditEventPage";
+import RootLayout from "./pages/Root";
+
+const router = createBrowserRouter([
+    {path:'/',element: <RootLayout/>, children:[
+            {index: true, element: <HomePage/>},
+            {path: 'events', element: <EventsPage/>},
+            {path: 'events/:id', element: <EventDetailPage/>},
+            {path: 'events/new', element: <NewEventPage/>},
+            {path: 'events/:id/edit', element: <EditEventPage/>}
+        ]},
+])
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router}/>;
 }
 
 export default App;
---
+++ frontend/src/pages/EventsPage.js
@@ -0,0 +1,5 @@
+const EventPage = () => {
+    return <h1>EventPage</h1>
+}
+
+export default EventPage;
